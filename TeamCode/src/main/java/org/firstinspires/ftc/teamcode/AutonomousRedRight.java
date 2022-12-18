f6ddaf1da1d9fdf2b4cd7924bde484e951004e1b
mpdavy <mpdavy@gmail.com>
2022-12-18 14:46:34 -0700

    Fixing minor bugs found during testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedRight.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TrajectorySegment;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -15,24 +18,27 @@ public class AutonomousRedRight extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         // We want to start the bot at x: 10, y: -8, heading: 90 degrees
-        Pose2d startPose = new Pose2d(10, -8, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(0, 0, 0);
         drive.setPoseEstimate(startPose);
 
         // define a series of trajectories
+        /*
         Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
+                .splineTo(new Vector2d(20, 20), Math.toRadians(-90) )
                 .build();
+        */
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
+        TrajectorySequence traj_seq = drive.trajectorySequenceBuilder(startPose)
+                .forward(20)
+                .strafeRight(20)
                 .build();
 
         waitForStart();
-
         if(isStopRequested()) return;
 
         // run my trajectories in order
-        drive.followTrajectory(traj1);
-        drive.followTrajectory(traj2);
+        // drive.followTrajectory(traj1);
+        drive.followTrajectorySequence(traj_seq);
+        // drive.turn(Math.toRadians(90));
     }
 }
