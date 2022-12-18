22ce064e49536986884b8cdd2a6828f9ddf337bb
mpdavy <mpdavy@gmail.com>
2022-12-18 12:17:10 -0700

    Simple demo of trajectories as a starting point.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedRight.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+
+@Autonomous(name = "Auto Red Right", group = "Linear Opmode")
+public class AutonomousRedRight extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        // We want to start the bot at x: 10, y: -8, heading: 90 degrees
+        Pose2d startPose = new Pose2d(10, -8, Math.toRadians(90));
+        drive.setPoseEstimate(startPose);
+
+        // define a series of trajectories
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
+                .build();
+
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        // run my trajectories in order
+        drive.followTrajectory(traj1);
+        drive.followTrajectory(traj2);
+    }
+}
