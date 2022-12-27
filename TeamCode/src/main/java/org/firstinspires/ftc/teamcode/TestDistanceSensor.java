63f80a8929b38a1adb229e8b3ef6b9b43d5606e9
mpdavy <mpdavy@gmail.com>
2022-12-27 14:09:38 -0700

    Setting up auto-centering test in mecanumDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDistanceSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDistanceSensor.java
@@ -29,132 +29,61 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.yise.mecanumDrive;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@TeleOp(name="Distance sensor", group="Linear Opmode")
+@TeleOp(name="Test AutoCentering", group="Linear Opmode")
 public class TestDistanceSensor extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
-
-    private Rev2mDistanceSensor distanceSensorRight = null;
-    private Rev2mDistanceSensor distanceSensorLeft = null;
-
-    public float speedMultiplier = 1;
+    public boolean canSwitchModes = false;
 
     @Override
     public void runOpMode() {
 
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // When you first test your robot, push the left joystick forward
-        // and flip the direction ( FORWARD <-> REVERSE ) of any wheel that runs backwards
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        distanceSensorRight = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor_right");
-        distanceSensorLeft = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor_left");
-
-        // Wait for the game to start (driver presses PLAY)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        mecanumDrive drive = new mecanumDrive(hardwareMap);
 
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max;
 
-            double distanceRight = distanceSensorRight.getDistance(DistanceUnit.CM);
-            double distanceLeft = distanceSensorLeft.getDistance(DistanceUnit.CM);
+            // -----------------------------------------------------------------------------------
+            // Drive Code
+            // -----------------------------------------------------------------------------------
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double vertical   = gamepad1.right_stick_x;  // Note: pushing stick forward gives negative value
-            double horizontal =  gamepad1.left_stick_x;
-            double turn     =  -gamepad1.left_stick_y;
-
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = vertical + horizontal + turn;
-            double rightFrontPower = vertical - horizontal - turn;
-            double leftBackPower   = vertical - horizontal + turn;
-            double rightBackPower  = vertical + horizontal - turn;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+            // Handle changing between drive modes
+            if (!gamepad1.y) {
+                canSwitchModes = true;
+            }
+            if (gamepad1.y && (drive.currentSpeed == mecanumDrive.Speeds.NORMAL) && canSwitchModes) {
+                drive.setSlowMode();
+            } else if (gamepad1.y && (drive.currentSpeed == mecanumDrive.Speeds.SLOW) && canSwitchModes) {
+                drive.setNormalMode();
             }
 
-            // Send calculated power to wheels
-            if (gamepad1.y && (speedMultiplier == 1)) {
-                speedMultiplier = 0.35f;
-            } else if (gamepad1.y && (speedMultiplier == 0.35f)){
-                speedMultiplier = 1f;
+            // If we have any Dpad input, update the motor power based on Dpad (ie overright stick)
+            if (gamepad1.dpad_right || gamepad1.dpad_left || gamepad1.dpad_up || gamepad1.dpad_down) {
+                drive.updateMotorsFromDpad(gamepad1);
+            } else {
+                drive.updateMotorsFromStick(gamepad1);
             }
 
-            leftFrontDrive.setPower(leftFrontPower * speedMultiplier);
-            rightFrontDrive.setPower(rightFrontPower * speedMultiplier);
-            leftBackDrive.setPower(leftBackPower * speedMultiplier);
-            rightBackDrive.setPower(rightBackPower * speedMultiplier);
-
+            // Auto-Centering Code
             if (gamepad1.left_trigger >= 0.8) {
-                if (distanceLeft < 20 && distanceRight < 20) {
-                    leftFrontDrive.setPower(0);
-                    rightBackDrive.setPower(0);
-                    leftBackDrive.setPower(0);
-                    rightFrontDrive.setPower(0);
-                } else if (distanceRight < 20) {
-                    leftFrontDrive.setPower(-0.2);
-                    rightBackDrive.setPower(-0.2);
-                    leftBackDrive.setPower(0.2);
-                    rightFrontDrive.setPower(0.2);
-                } else if (distanceLeft < 20) {
-                    leftFrontDrive.setPower(0.2);
-                    rightBackDrive.setPower(0.2);
-                    leftBackDrive.setPower(-0.2);
-                    rightFrontDrive.setPower(-0.2);
-                }else if ((distanceRight < 45 && distanceRight > 20) || (distanceLeft < 45 && distanceLeft > 20)) {
-                    leftFrontDrive.setPower(0.4);
-                    rightBackDrive.setPower(-0.4);
-                    leftBackDrive.setPower(0.4);
-                    rightFrontDrive.setPower(-0.4);
-                }
+                drive.autoCenter();
             }
 
             // Show the elapsed game time and wheel power.
-            //telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Distance right: ", distanceSensorRight.getDistance(DistanceUnit.CM));
-            telemetry.addData("Distance left ", distanceSensorLeft.getDistance(DistanceUnit.CM));
-            telemetry.update();
+            // mecanumDrive.updateDistances();
+            // telemetry.addData("Distance right: ", mecanumDrive.distanceLeft);
+            // telemetry.addData("Distance left ", mecanumDrive.distanceRight);
+            // telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yise/mecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yise/mecanumDrive.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode.yise;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class mecanumDrive {
     // Note: we make these public so the calling code can access and use these variables
     public final DcMotor leftFrontDrive, leftBackDrive, rightFrontDrive, rightBackDrive;
     public double leftFrontPower, leftBackPower, rightFrontPower, rightBackPower;
+
+    public final Rev2mDistanceSensor distanceSensorRight;
+    public final Rev2mDistanceSensor distanceSensorLeft;
+    public double distanceLeft;
+    public double distanceRight;
 
     // Used to track slow-mode versus normal mode
     public Speeds currentSpeed;
@@ -37,6 +44,14 @@ public class mecanumDrive {
         leftBackPower = 0.5;
         rightFrontPower = 0.5;
         rightBackPower = 0.5;
+
+        // set sensor variables based on hardware config
+        distanceSensorRight = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor_right");
+        distanceSensorLeft = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor_left");
+
+        // initialize the distance measurements using centimeters units
+        distanceLeft = distanceSensorLeft.getDistance(DistanceUnit.CM);
+        distanceRight = distanceSensorRight.getDistance(DistanceUnit.CM);
     }
 
     // Updates power to the 4 drive motors based on input from the stick on the first controller
@@ -129,6 +144,43 @@ public class mecanumDrive {
         currentSpeed = Speeds.NORMAL;
     }
 
+    public void autoCenter() {
+        // get a fresh copy of the current sensor readings
+        distanceLeft = distanceSensorLeft.getDistance(DistanceUnit.CM);
+        distanceRight = distanceSensorRight.getDistance(DistanceUnit.CM);
+
+        // if both sides are < 20cm, stop
+        if (distanceLeft < 20 && distanceRight < 20) {
+            leftFrontDrive.setPower(0);
+            rightBackDrive.setPower(0);
+            leftBackDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            // if only right side is < 20cm, drive right
+        } else if (distanceRight < 20) {
+            leftFrontDrive.setPower(0.3);
+            rightBackDrive.setPower(0.3);
+            leftBackDrive.setPower(-0.3);
+            rightFrontDrive.setPower(-0.3);
+            // if only the left side is < 20cm, drive left
+        } else if (distanceLeft < 20) {
+            leftFrontDrive.setPower(-0.3);
+            rightBackDrive.setPower(-0.3);
+            leftBackDrive.setPower(0.3);
+            rightFrontDrive.setPower(0.3);
+            // if either side is between 20cm <> 35cm, drive forward
+        } else if ((distanceRight < 35 && distanceRight > 20) || (distanceLeft < 35 && distanceLeft > 20)) {
+            leftFrontDrive.setPower(0.4);
+            rightBackDrive.setPower(0.4);
+            leftBackDrive.setPower(0.4);
+            rightFrontDrive.setPower(0.4);
+        }
+    }
+
+    public void updateDistances() {
+        // get a fresh copy of the current sensor readings
+        distanceLeft = distanceSensorLeft.getDistance(DistanceUnit.CM);
+        distanceRight = distanceSensorRight.getDistance(DistanceUnit.CM);
+    }
 }
 
 
