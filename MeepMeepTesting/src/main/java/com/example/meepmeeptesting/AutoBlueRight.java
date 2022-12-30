55e1ec1fa07a467673cc35713e897ab060f15f55
Sarah <FTCsjwingat@users.noreply.github.com>
2022-12-30 13:21:30 -0700

    auto blue right
    i made an autonomous  for blue right side

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoBlueRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoBlueRight.java
@@ -6,7 +6,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class AutoRedLeftTest {
+public class AutoBlueRight {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -14,32 +14,28 @@ public class AutoRedLeftTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(49.44, 49.44, Math.toRadians(180), Math.toRadians(180), 10.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36, -62, Math.toRadians(270)))
-                                .strafeRight(12)
-                                .splineToConstantHeading(new Vector2d(-48, -12), Math.toRadians(270))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 62, Math.toRadians(90)))
+                                .strafeRight(-12)
+                                .splineToConstantHeading(new Vector2d(-48, 12), Math.toRadians(90))
                                 .addDisplacementMarker(20, () -> {
                                    // arm.getTopCone();
                                 })
                                 .addDisplacementMarker(20, () -> {
                                    // coneGrabber.setPosition(Servo.MIN_POSITION);
                                 })
-                                .turn(Math.toRadians(-90))
+                                .turn(Math.toRadians(90))
                                 .forward(12)
                                 .addTemporalMarker(2, () -> {
                                     //yiseDrive.autoCenter();
                                 })
-                                //.waitSeconds(2)
                                 .addTemporalMarker(2,() -> {
                                     //coneGrabber.setPosition(Servo.MAX_POSITION);
                                 })
-                                //.waitSeconds(2)
                                 .addTemporalMarker(2, () -> {
                                     //arm.setPoleHeight(liftArm.Heights.HIGH);
                                 })
-                                //.waitSeconds(2)
-                                //SEQ2
-                                .lineToConstantHeading(new Vector2d(-11, -14))
-                                .turn(Math.toRadians(135))
+                                .lineToConstantHeading(new Vector2d(-11, 14))
+                                .turn(Math.toRadians(-135))
                                 .forward(6)
                                 .addTemporalMarker(.2,() -> {
                                    // yiseDrive.autoCenter();
@@ -48,17 +44,16 @@ public class AutoRedLeftTest {
                                 .addTemporalMarker(.2, () -> {
                                     //coneGrabber.setPosition(Servo.MIN_POSITION);
                                 })
-                                //SEQ3
-                                .turn(Math.toRadians(-135))
-                                .lineToConstantHeading(new Vector2d(-52, -12))
+                                .turn(Math.toRadians(135))
+                                .lineToConstantHeading(new Vector2d(-52, 12))
                                 .addDisplacementMarker(20, () -> {
-                                   // arm.getTopCone();
-                                   // arm.downOneCone();
+                                    //arm.getTopCone();
+                                    //arm.downOneCone();
                                 })
                                 .addTemporalMarker(() -> {
                                     //yiseDrive.autoCenter();
                                 })
-                                //.waitSeconds(.2)
+                                .waitSeconds(.2)
                                 .build()
 
                 );
