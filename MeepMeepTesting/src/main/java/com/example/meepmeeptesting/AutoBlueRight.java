9fbe54c0e458f14443714946cef0ea7fda7a918f
Sarah <FTCsjwingat@users.noreply.github.com>
2023-02-07 21:06:53 -0700

    added in new auto for parking
    added in new auto programs for just getting park

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoBlueRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoBlueRight.java
@@ -14,70 +14,11 @@ public class AutoBlueRight {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(49.44, 49.44, Math.toRadians(180), Math.toRadians(180), 10.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36, 62, Math.toRadians(90)))
-                                .strafeRight(-12)
-                                .splineToConstantHeading(new Vector2d(-48, 14), Math.toRadians(90))
-                                .addDisplacementMarker(20, () -> {
-                                    //arm.getTopCone();
-                                })
-                                .addDisplacementMarker(20, () -> {
-                                    //arm.openGrabber();
-                                })
-                                .turn(Math.toRadians(90))
-                                .forward(10)
-                                .addTemporalMarker(() -> {
-                                    //yiseDrive.autoCenterLoop(mecanumDrive.centerModes.CONE);
-                                })
-                                .addTemporalMarker(() -> {
-                                    //arm.closeGrabber();
-                                })
-                                .waitSeconds(1.5)
-                                .addTemporalMarker(() -> {
-                                    //arm.setPoleHeight(liftArm.Heights.HIGH);
-                                })
-
-                                //SEQ_2
-                                .lineToConstantHeading(new Vector2d(-11, 14))
-                                .turn(Math.toRadians(-140))
-                                .forward(8)
-                                .addTemporalMarker(() -> {
-                                    //yiseDrive.autoCenterLoop(mecanumDrive.centerModes.POLE);
-                                })
+                        drive.trajectorySequenceBuilder(new Pose2d(33, -62, Math.toRadians(-100)))
                                 .waitSeconds(.2)
-                                .addTemporalMarker(() -> {
-                                    //arm.openGrabber();
-                                })
-                                .waitSeconds(.4)
-                                .back(8)
-
-                                //END SEQ
-                                /* if (coneNumber == 3){
-                                     endLocation_X = -58;
-                                     endLocation_Y = 14;
-                                     endHeading_Z = 180;
-                                 } else if (coneNumber == 2){
-                                     endLocation_X = -34;
-                                     endLocation_Y = 16;
-                                     endHeading_Z = 0;
-                                 } else if (coneNumber == 1){
-                                     endLocation_X = -12;
-                                     endLocation_Y = 16;
-                                     endHeading_Z = 0;
- }*/
-                                //.lineToLinearHeading(new Pose2d( endLocation_X, endLocation_Y,  Math.toRadians(endHeading_Z)))
-                                //.lineToLinearHeading(new Pose2d(-58, 14, Math.toRadians(-0)))  //location = 3
-                                .lineToLinearHeading(new Pose2d(-34, 16, Math.toRadians(0))) // location = 2
-                                //.lineToLinearHeading(new Pose2d(-12, 16, Math.toRadians(0)))  //location = 1
-                                .addTemporalMarker(() ->{
-                                    //arm.closeGrabber();
-                                })
-                                .addTemporalMarker(() ->{
-                                    //arm.returnToBottom();
-                                })
-                                .turn(Math.toRadians(90))
-
-                                //to see end point in meep meep
-                                .waitSeconds(5)
+                                .lineToLinearHeading(new Pose2d(36,-11, Math.toRadians(-90)))
+                                .lineToLinearHeading(new Pose2d(36, -22, Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(63,-13, Math.toRadians(-5)))
                                 .build()
 
                 );
